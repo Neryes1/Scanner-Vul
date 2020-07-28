49f705e1f13a8790904e400314414ddd311575f6
Herbman <brunery@gmail.com>
2020-07-28 17:38:00 -0300

    :pencil:Documentação

--- scan.py
+++ scan.py
@@ -1,43 +1,36 @@
-#from mac_vendor_lookup import MacLookup
+''' 
+  Projeto de TCC com o objetivo de escanear a rede interna onde contém todos 
+  os dispositivos inteligentes. Essa análise vai jogar na tela os hosts, fabricantes
+  do MAC e os serviços rodando.
+'''
 
+#importando a biblioteca nmap
 import nmap
+
+'''
+  criando objeto n que reseberá o método PortScanner da 
+  biblioteca nmap
+'''
 n = nmap.PortScanner()
 
-#função para pegar os valores dentro do tcp   
+#função para resgatar os valores dentro do tcp   
 def extract_names_from(tcp):
   for val in tcp.values():
     print('Serviço: ', val["name"])
 
+'''
+  o método scan passa 3 parâmentros
+  localhost = ip da rede
+  arguments: -n = para não resolver DNS e -PU = descoberta de host
+  sudo = pedir permissão
+'''
 n.scan('192.168.1.0/24', arguments='-n -PU', sudo=True)
 
-'''
+
 for host in n.all_hosts():
   print(' ')    
   print('IP : %s (%s)' % (host, n[host].hostname()))
   if 'mac' in n[host]['addresses']:
     print('Fabricante: {}'.format(" ".join(n[host]['vendor'].values())))
   extract_names_from(n[host]["tcp"])
-
-
-'''        
-for host in n.all_hosts():
-  if 'host':
-    print(' ')    
-    print('IP : %s (%s)' % (host, n[host].hostname()))
-    extract_names_from(n[host]["tcp"])
-  else:
-    print('Nada encontrado')
-
-
-
-
-# inserindo MAC na mão para conferir com o resultado de cima
-'''
-print('================') 
-
-  print(MacLookup().lookup("CC:06:77:F3:DD:50"))
-  print(MacLookup().lookup("DC:35:F1:42:85:33"))
-  print(MacLookup().lookup("30:4B:07:8F:55:F1"))
-  print(MacLookup().lookup("40:B0:76:1C:DE:CD"))
-'''
 
